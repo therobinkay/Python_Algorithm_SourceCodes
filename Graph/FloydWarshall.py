ab3e1f9f52bd9b8ce907f2856bcf03ed87f8ffd5
H. Robin Kong <hskongx@gmail.com>
2021-12-17 15:47:27 +0900

    Update FloydWarshall.py

--- Graph/FloydWarshall.py
+++ Graph/FloydWarshall.py
@@ -11,7 +11,7 @@ for a in range(1, n+1):
         if a == b:
             graph[a][b] = 0
 
-# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+# input all edge info and update the values as the input value
 for _ in range(m):
     # set C as a cost to go from A to B
     a, b, c = map(int, input().split())
